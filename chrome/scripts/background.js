106e137d5d1bf78554d760203692cee5d7e5f645
cssxsh <cssxsh@gmail.com>
2019-10-03 07:38:29 +0800

    permissions rules set

--- chrome/scripts/background.js
+++ chrome/scripts/background.js
@@ -1,21 +1,43 @@
 var back = {
     assetFilter: {},
-    getFilter: function () {
+    loadFilter: function (func) {
         chrome.storage.sync.get("assetFilter", result => {
-            if (result == null) return;
 
             back.assetFilter = result.assetFilter;
+            if (func != null) func(back.assetFilter);
         });
     },
     start: function () {
-        back.webListener.closeCORS();
-        back.webListener.closeCSP();
-        chrome.storage.sync.get("assetFilter", result => {
-            if (result == null) return;
-
-            back.assetFilter = result.assetFilter;
-            back.webListener.add();
+        back.loadFilter(assetFilter => {
+            chrome.permissions.contains(
+            { 
+                origins: assetFilter.origins.concat(assetFilter.filter.urls)
+            }, 
+            result => {
+                if (result) {
+                    back.webListener.add();
+                } else {
+                    alert("Permission denied!");
+                }
+            });
         });
+    },
+    closeCORS: details => {
+        details.responseHeaders.push({name: "Access-Control-Allow-Origin", value: "*"});
+        details.responseHeaders.push({name: "Access-Control-Allow-Methods", value: "GET"});
+        details.responseHeaders.push({name: "Access-Control-Max-Age", value: "0"});
+
+        return { responseHeaders: details.responseHeaders };
+    },
+    closeCSP: details => {
+        for (let i in details.responseHeaders) {
+            if ("CONTENT-SECURITY-POLICY" == details.responseHeaders[i].name.toUpperCase()) {
+                // TODO 这里应该是添加
+                details.responseHeaders[i].value = "";
+                
+                return { responseHeaders: details.responseHeaders };
+            }
+        }
     },
     redirect: request => {
         let url = request.url;
@@ -36,25 +58,45 @@ var back = {
     },
     webListener: {
         add: function () {
-            chrome.webRequest.onBeforeRequest.addListener(
-                back.redirect, 
+            // closeCSP
+            if (back.assetFilter.switch.closeCSP) chrome.webRequest.onHeadersReceived.addListener(
+                back.closeCSP, 
                 {
-                    urls: back.assetFilter.urls,
+                    urls: back.assetFilter.origins,
                     types: [
-                        // "main_frame", 
-                        // "sub_frame", 
+                        "main_frame", 
+                        "sub_frame", 
                         // "stylesheet", 
-                        "script", 
+                        // "script", 
                         // "image", 
                         // "font", 
                         // "object", 
                         // "xmlhttprequest", 
                         // "ping", 
                         // "csp_report", 
-                        // "media",
+                        // "media", 
+                        // "websocket", 
                         // "other"
                     ]
-                },
+                }, 
+                [
+                    "blocking", 
+                    "responseHeaders"
+                ]
+            );
+            // closeCORS
+            if (back.assetFilter.switch.closeCORS) chrome.webRequest.onHeadersReceived.addListener(
+                back.closeCORS, 
+                back.assetFilter.filter,
+                [
+                    "blocking", 
+                    "responseHeaders"
+                ]
+            );
+            // redirect
+            chrome.webRequest.onBeforeRequest.addListener(
+                back.redirect, 
+                back.assetFilter.filter,
                 [
                     "blocking",
                 ]
@@ -64,62 +106,12 @@ var back = {
             chrome.webRequest.onBeforeRequest.removeListener(
                 back.redirect
             );
-        },
-        closeCORS: function () {
-            chrome.webRequest.onHeadersReceived.addListener(
-                details => {
-                    details.responseHeaders.push({name: "Access-Control-Allow-Origin", value: "*"});
-                    details.responseHeaders.push({name: "Access-Control-Allow-Methods", value: "GET"});
-                    details.responseHeaders.push({name: "Access-Control-Max-Age", value: "0"});
-
-                    return { responseHeaders: details.responseHeaders };
-                }, 
-                {
-                    urls: [
-                        "<all_urls>"
-                    ],
-                    types: [
-                        "script"
-                    ]
-                }, 
-                [
-                    "blocking", 
-                    "responseHeaders"
-                ]
+            
+            if (back.assetFilter.closeCORS) chrome.webRequest.onHeadersReceived.removeListener(
+                back.closeCORS
             );
-        },
-        closeCSP: function () {
-            chrome.webRequest.onHeadersReceived.addListener(
-                details => {
-                    if (details.type == "script") {
-                        details.responseHeaders.push({name: "Access-Control-Allow-Origin", value: "*"});
-                        details.responseHeaders.push({name: "Access-Control-Allow-Methods", value: "GET"});
-                        details.responseHeaders.push({name: "Access-Control-Max-Age", value: "0"});
-
-                        return { responseHeaders: details.responseHeaders };
-                    }
-                    for (let i in details.responseHeaders) {
-                        if ("CONTENT-SECURITY-POLICY" == details.responseHeaders[i].name.toUpperCase()) {
-                            // TODO 这里应该是添加
-                            details.responseHeaders[i].value = "";
-                            
-                            return { responseHeaders: details.responseHeaders };
-                        }
-                    }
-                }, 
-                {
-                    urls: [
-                        "<all_urls>"
-                    ],
-                    types: [
-                        "main_frame", 
-                        "sub_frame"
-                    ]
-                }, 
-                [
-                    "blocking", 
-                    "responseHeaders"
-                ]
+            if (back.assetFilter.closeCSP) chrome.webRequest.onHeadersReceived.removeListener(
+                back.closeCSP
             );
         }
     }
--- chrome/scripts/options.js
+++ chrome/scripts/options.js
@@ -1,21 +1,62 @@
-chrome.storage.sync.get("assetFilter", result => {
-    document.getElementById("jsonInput").value = JSON.stringify(result, null, 4);
-});
-
-document.getElementById('btn_submit').addEventListener('click',
-    function saveChanges() {
-        textarea = document.getElementById("jsonInput");
-        var theValue = textarea.value;
-
+var options = {
+    result: null,
+    init: function () {
+        // 初始化数据
+        chrome.storage.sync.get("assetFilter", result => {
+            options.result = result;
+            document.getElementById("jsonInput").value = JSON.stringify(options.result, null, 4);
+        });
+        // 绑定事件
+        document.getElementById("btn_submit").addEventListener("click", options.save);
+    },
+    save: event => {
+        let theValue = document.getElementById("jsonInput").value;
+    
         if (!theValue) {
+            alert("Can't be null!");
             return;
         }
-        theValue = JSON.parse(theValue);
-        chrome.storage.sync.set(theValue, function() {
-            let back = chrome.extension.getBackgroundPage().back;
-            back.getFilter();
-            back.webListener.move();
-            back.webListener.add();
+        try {
+            theValue = JSON.parse(theValue);
+        } catch(error) {
+            alert(error);
+            return;
+        }
+
+        // 更新权限
+        let oldResult = options.assetFilter;
+        let newResult = theValue.assetFilter;
+        let oldOrigins = "";
+        try {
+            oldOrigins = oldResult.origins.concat(oldResult.filter.urls);
+            chrome.permissions.remove({ origins: oldOrigins });
+        } catch(error) {
+            // 原值不规范
+        }
+        let newOrigins = newResult.origins.concat(newResult.filter.urls);
+        chrome.permissions.request(
+            { 
+                origins: newOrigins,
+                permissions: [
+                    "webRequest",
+                    "webRequestBlocking"
+                ]
+            }, 
+            granted => {
+            if (!granted) {
+                alert("Save failed!");
+            } else {
+                // 更新设置
+                chrome.storage.sync.set(theValue, function() {
+                    let back = chrome.extension.getBackgroundPage().back;
+                    back.loadFilter();
+                    back.webListener.move();
+                    back.webListener.add();
+                });
+            }
         });
     }
-);
+};
+
+// 初始化
+options.init();
